20cda8e6fc56d4be09a9cc8321b23fd582f7b6b3
Amarjit Kumar <amarjitkr0511@gmail.com>
2023-03-18 14:07:44 +0530

    Synced time with machine's time

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function updateClock() {
     const minute = currentDate.getMinutes();
     const second = currentDate.getSeconds();
     const hourDeg = (hour/12)*360;
-    hourE1.style.transform ='rotate(${hourDeg}deg)';
+    hourE1.style.transform =`rotate(${hourDeg}deg)`;
     const minuteDeg = (minute/60)*360;
-    minuteE1.style.transform = 'rotate(${minuteDeg}deg)';
+    minuteE1.style.transform = `rotate(${minuteDeg}deg)`;
     const secondDeg = (second / 60)*360;
-    secondE1.style.transform = 'rotate(${secondDeg}deg)';
+    secondE1.style.transform = `rotate(${secondDeg}deg)`;
 
 }
 
